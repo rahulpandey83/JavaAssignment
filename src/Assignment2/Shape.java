bbb11e83c9ff769ab8070e9b4cb5e7804ba8669d
Rahul <rahulpandey8397136@gmail.com>
2021-12-01 16:21:19 +0530

    Worked on Assignment 2 and Implemented Calculation of different shapes

---
+++ src/Assignment2/Shape.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public interface Shape {
+	void calculateAreaAndPerimeterOfCircle();
+	void calculateAreaAndPerimeterOfTriangle();
+	void calculateAreaAndPerimeterOfRectangle();
+	void calculateAreaAndPerimeterOfSquare();
+	void calculateSurfaceAreaAndVolumeOfSphere();
+	void calculateSurfaceAreaAndVolumeOfCylinder();
+	
+
+}
